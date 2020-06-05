dd6fae2cd9e2999e3f32cea876f844a034bc9278
Daxit Patel <52673729+elliotaldersonn@users.noreply.github.com>
2020-06-05 11:38:47 +0530

    1 error handled

--- paytm/views.py
+++ paytm/views.py
@@ -5,19 +5,24 @@ from django.views.decorators.csrf import csrf_exempt
 from .checksum import verify_checksum
 from msg.models import Message
 from .models import Transaction
+from django.http import Http404
 
 # Create your views here.
 
 @csrf_exempt
 def handleresponse(request):
 	#paytm will send response here
-	form = request.POST
-	response_dict = {}
-	for i in form.keys():
-		response_dict[i] = form[i]
-		if i == 'CHECKSUMHASH':
-			checksum = form[i]
-	verify = verify_checksum(response_dict, settings.MKEY ,checksum)
+	try:
+		form = request.POST
+		response_dict = {}
+		for i in form.keys():
+			response_dict[i] = form[i]
+			if i == 'CHECKSUMHASH':
+				checksum = form[i]
+		verify = verify_checksum(response_dict, settings.MKEY ,checksum)
+	#returns 404 if user tries to directly access '/transaction/'
+	except:
+		raise Http404
 	if verify:
 		if response_dict['RESPCODE'] == '01':
 			#transaction success
